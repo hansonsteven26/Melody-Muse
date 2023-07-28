5d762cd373d32718e46fca19f472284ebfe39ccc
Emily Caton <caton.emily@yahoo.com>
2023-07-27 20:03:58 -0500

    fetched data with both api's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
-// const tmKey="ikLGPMmwldjXqMOy8UwPoAs5lxgKfbhA"
-// const tmUrl="https://app.ticketmaster.com/discovery/v2/events.json?postalCode&countryCode=US"
+const dogKey="live_FTU78RuBy8JufSDO0cYsxxEjUweRpQ8Vaa7oRI682TC2gEVMzBSGe1WxqUxkiDjj"
+const dogUrl="https://api.thedogapi.com/v1/breeds?breed_ids=1"
 
-// async function checkEvents(){
-//     const response = await fetch(tmUrl + `&apikey=${tmKey}`);
-//      let data = await response.json();
+async function checkEvents(){
+    const response = await fetch(dogUrl + `&apikey=${dogKey}`);
+     let data = await response.json();
 
-//     console.log(data);
-// }
-// checkEvents();
+    console.log(data);
+}
+checkEvents();
 
+var breedName = 'poodle'
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/animals?name=' + breedName,
+    headers: { 'X-Api-Key': '9otJVZp5fJ9qMc2fEsmc/g==YNaJYpvMld2P9utw'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
