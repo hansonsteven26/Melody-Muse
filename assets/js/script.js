56eb87c681613b2ec8fa40b249b4ad417ccfedf6
Emily Caton <caton.emily@yahoo.com>
2023-07-31 21:44:13 -0500

    pull

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-const dogKey="live_FTU78RuBy8JufSDO0cYsxxEjUweRpQ8Vaa7oRI682TC2gEVMzBSGe1WxqUxkiDjj"
-const dogUrl="https://api.thedogapi.com/v1/breeds?breed_ids=1"
-let imageEl= document.getElementById("card-1")
-let breedImage=document.getElementById("breedImage")
-let searchEl = document.getElementById("breedInput")
-=======
 const dogKey = "live_FTU78RuBy8JufSDO0cYsxxEjUweRpQ8Vaa7oRI682TC2gEVMzBSGe1WxqUxkiDjj"
 const dogUrl = "https://api.thedogapi.com/v1/breeds?breed_ids=1"
 let imageEl = document.getElementById("card-1")
@@ -40,7 +33,6 @@ searchButton.addEventListener("click", function () {
         }
 
     });
->>>>>>> 4c2e13628bb28ee3583720e8eb6384339e0f9d9d
 
 })
 
@@ -52,21 +44,3 @@ async function checkEvents() {
 checkEvents();
 
 
-<<<<<<< HEAD
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/dogs?name=' + breedName,
-    headers: { 'X-Api-Key': '9otJVZp5fJ9qMc2fEsmc/g==YNaJYpvMld2P9utw'},
-    contentType: 'application/json',
-    success: function(result) {
-    for(let i = 0; i < 3; i++) {
-        let imageLink= result[i].image_link;
-        document.getElementById('card-' +i).setAttribute("src", imageLink);
-        
-    }
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }}
-});
-=======
->>>>>>> 4c2e13628bb28ee3583720e8eb6384339e0f9d9d
