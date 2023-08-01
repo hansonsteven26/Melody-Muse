2d4c239ab2055e3d01cb479635c1e3047c8d437d
Emily Caton <caton.emily@yahoo.com>
2023-07-31 21:17:48 -0500

    merge error

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const dogKey="live_FTU78RuBy8JufSDO0cYsxxEjUweRpQ8Vaa7oRI682TC2gEVMzBSGe1WxqUxki
 const dogUrl="https://api.thedogapi.com/v1/breeds?breed_ids=1"
 let imageEl= document.getElementById("card-1")
 let breedImage=document.getElementById("breedImage")
+let searchEl = document.getElementById("breedInput")
 
 let searchButton= document.getElementById("searchButton");
 searchButton.addEventListener("click",function(){
@@ -15,7 +16,7 @@ async function checkEvents(){
 }
 checkEvents();
 
-var breedName = 'poodle'
+
 $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/dogs?name=' + breedName,
@@ -25,9 +26,7 @@ $.ajax({
     for(let i = 0; i < 3; i++) {
         let imageLink= result[i].image_link;
         document.getElementById('card-' +i).setAttribute("src", imageLink);
-        console.log(imageEl.getAttribute("src"))
-
-        console.log(result[0].image_link)
+        
     }
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
